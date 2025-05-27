078c31399d1f50e0c2f238e57762e69412e9a6e2
Rilwan Jibri <rilwangabi10@gmail.com>
2025-05-27 01:54:18 +0100

    React-template

--- react/notes-spa/src/main.tsx
+++ react/notes-spa/src/main.tsx
@@ -1,25 +1,15 @@
-// main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-// import Home from './pages/Home';
-// import About from './pages/About';
 import "./index.css";
 
 // Create a root and render the application inside a strict mode wrapper
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    {/* Set up routing using BrowserRouter */}
     <BrowserRouter>
       <Routes>
-        {/* Root route wraps all child routes */}
-        <Route path="/" element={<App />}>
-          {/* Index route: rendered at the base path '/' */}
-          {/* <Route index element={<Home />} /> */}
-          {/* '/about' route for the About page */}
-          {/* <Route path="about" element={<About />} /> */}
-        </Route>
+        <Route path="/" element={<App />}></Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
